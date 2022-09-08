4316a3414075e6b966ad34b781545d700a306bbe
mercuri <mercuri90028@gmail.com>
2022-09-08 17:08:38 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,14 @@ import React from 'react'
 import '../styles/App.css';
 const App = () => {
 
+  const handleSubmit =(event)=>{
+    event.preventDefault();
+    console.log("form submitted");
+  }
 
   return (
     <div id="main">
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor='name'>Name</label>
         <input id="name" type={"text"} />
 
